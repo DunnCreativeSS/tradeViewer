26ff9b62be2392d5a8c52d23135e3faad38965a0
DunnCreativeSS <jarett+github@dcssquared.com>
2018-07-05 22:08:18 -0300

    first commit

--- app.js
+++ app.js
@@ -487,6 +487,7 @@ var msg = '<head><meta http-equiv="refresh" content="5"><script src="https://aja
 					console.log(diffB)
 					console.log((diffA + diffB ) /2)
 					if ((diffA + diffB ) /2 > 0.05 || (diffA + diffB ) /2 < -0.05){
+						if ((diffA + diffB ) /2 <= 10 || (diffA + diffB ) /2 >= -10){
 						if ((diffA + diffB ) /2 > 0){
 						msg+="<tr style = 'color:red;'>"
 						msg+="<td><form id='" + p + "'style='display:none;' action='/' method='post'><input type='hidden' name='Input' value='" + p + "'></input></form><a href='#' onclick='$(\"#" + p + "\").submit()'>" + p + "</a></td>"
@@ -511,6 +512,7 @@ var msg = '<head><meta http-equiv="refresh" content="5"><script src="https://aja
 					msg+="<td>"+ diffB + "</td>"
 						msg+="</tr>"
 					}
+						}
 					}
 				
 				}
